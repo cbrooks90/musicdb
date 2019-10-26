cd5f8a6b121ffdbde12fa8537074b0b784ae7796
Christopher Brooks <cbrooks90@gmail.com>
2019-10-26 13:04:31 -0500

    Schema

--- tables.sql
+++ tables.sql
@@ -1,11 +1,13 @@
-CREATE TABLE composers (
+CREATE SCHEMA music;
+
+CREATE TABLE music.composers (
 	id integer PRIMARY KEY,
 	name character varying(255) NOT NULL,
 	birthdate date NOT NULL,
 	dateerror integer NOT NULL
 );
 
-CREATE TABLE pieces (
+CREATE TABLE music.pieces (
 	id integer PRIMARY KEY,
 	title character varying(255) NOT NULL,
 	composer integer NOT NULL
@@ -14,13 +16,13 @@ CREATE TABLE pieces (
 
 -- Opus numbers are not necessarily unique; see Busoni for example.
 
-CREATE TABLE opusnumbers (
+CREATE TABLE music.opus_numbers (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
 	opus integer NOT NULL
 );
 
-CREATE TABLE catalogs (
+CREATE TABLE music.catalogs (
 	id integer PRIMARY KEY,
 	composer integer NOT NULL
 		REFERENCES composers ON UPDATE CASCADE,
@@ -28,7 +30,7 @@ CREATE TABLE catalogs (
 	abbrev character varying(8) NOT NULL
 );
 
-CREATE TABLE catalognumbers (
+CREATE TABLE music.catalog_numbers (
 	piece integer NOT NULL
 		REFERENCES pieces ON UPDATE CASCADE,
 	cat integer NOT NULL
@@ -38,7 +40,7 @@ CREATE TABLE catalognumbers (
 	PRIMARY KEY (piece, cat)
 );
 
-CREATE TABLE sequences (
+CREATE TABLE music.pieces_in_sequence (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
 	parent integer NOT NULL
@@ -48,13 +50,13 @@ CREATE TABLE sequences (
 	UNIQUE (parent, num)
 );
 
-CREATE TABLE cycles (
+CREATE TABLE music.cycles (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
 	num integer NOT NULL
 );
 
-CREATE TABLE indications (
+CREATE TABLE music.indications (
 	piece integer NOT NULL
 		REFERENCES pieces ON UPDATE CASCADE,
 	num integer NOT NULL,
@@ -63,41 +65,41 @@ CREATE TABLE indications (
 	PRIMARY KEY (piece, num)
 );
 
-CREATE TABLE adapttypes (
+CREATE TABLE music.adaptation_types (
 	id integer PRIMARY KEY,
 	name character varying(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE adaptations (
+CREATE TABLE music.adaptations (
 	piece integer NOT NULL
 		REFERENCES pieces ON UPDATE CASCADE,
 	original integer NOT NULL
 		REFERENCES pieces ON UPDATE CASCADE,
-	adapttype integer NOT NULL
-		REFERENCES adapttypes ON UPDATE CASCADE,
+	adaptation_type integer NOT NULL
+		REFERENCES adaptation_types ON UPDATE CASCADE,
 
 	PRIMARY KEY (piece, original)
 );
 
-CREATE TABLE completionyears (
+CREATE TABLE music.completion_years (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
 	completed integer NOT NULL
 );
 
-CREATE TABLE keys (
+CREATE TABLE music.keys (
 	id character varying(8) PRIMARY KEY,
 	name character varying(255) UNIQUE NOT NULL
 );
 
-CREATE TABLE pieceswithkey (
+CREATE TABLE music.pieces_with_key (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
-	keysignature character varying(8) NOT NULL
+	key_signature character varying(8) NOT NULL
 		REFERENCES keys ON UPDATE CASCADE
 );
 
-CREATE TABLE subtitles (
+CREATE TABLE music.subtitles (
 	piece integer PRIMARY KEY
 		REFERENCES pieces ON UPDATE CASCADE,
 	subtitle character varying(255) NOT NULL
